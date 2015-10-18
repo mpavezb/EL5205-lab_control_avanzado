eeefc83949011601606e7558d274890c8079facc
Pablo Saavedra <pablo.saavedra@ug.uchile.cl>
2015-10-18 10:12:32 -0200

    Pequeños cambios en Simulink del modelo linealizado

--- exp2/cal_polos.m
+++ exp2/cal_polos.m
@@ -6,8 +6,8 @@ format shortG
 format compact
 addpath('lib', 'mat','sim');
 
-load('mat/discrete_sys.mat');
-load('mat/obs_luenberger.mat');
+load('mat/discrete_sys_int_theta.mat');
+load('mat/obs_luenberger_int_theta.mat');
 
 sys_d = ss(Ad, Bd, Cd, Dd, Ts); % generar sistema lineal
 [poles_d,zeros_d] = pzmap(sys_d); % polos y ceros del sistema linealizado
--- exp2/lib/plot_error.m
+++ exp2/lib/plot_error.m
@@ -1,11 +1,12 @@
 function [ ] = plot_error( ELD, ELD2, title_plot )
 %PLOT_ERROR Summary of this function goes here
 %   Detailed explanation goes here
+    N = size(ELD,2);
     figure; hold on;
     subplot(221);plot(ELD(:,1), ELD(:,2) - ELD2(:,2),'-r','displayname','$\alpha$'); xlabel('Tiempo [s]'); ylabel('Angulo [rad]');hlegend1 = legend(gca, 'show', 'location', 'best'); set(hlegend1,'Interpreter','latex');
     subplot(223);plot(ELD(:,1), ELD(:,3) - ELD2(:,3),'-b','displayname','$\theta$'); xlabel('Tiempo [s]'); ylabel('Angulo [rad]');hlegend1 = legend(gca, 'show', 'location', 'best'); set(hlegend1,'Interpreter','latex');
     subplot(222);plot(ELD(:,1), ELD(:,4) - ELD2(:,4),'-r','displayname','$\dot{\alpha}$'); xlabel('Tiempo [s]'); ylabel('Vel. Angular [rad/s]');hlegend1 = legend(gca, 'show', 'location', 'best'); set(hlegend1,'Interpreter','latex');
-    subplot(224);plot(ELD(:,1), ELD(:,5) - ELD2(:,5),'-b','displayname','$\dot{\theta}$'); xlabel('Tiempo [s]'); ylabel('Vel. Angular [rad/s]');hlegend1 = legend(gca, 'show', 'location', 'best'); set(hlegend1,'Interpreter','latex');
+    subplot(224);plot(ELD(:,1), ELD(:,N) - ELD2(:,N),'-b','displayname','$\dot{\theta}$'); xlabel('Tiempo [s]'); ylabel('Vel. Angular [rad/s]');hlegend1 = legend(gca, 'show', 'location', 'best'); set(hlegend1,'Interpreter','latex');
     suptitle(title_plot);
 end
 
