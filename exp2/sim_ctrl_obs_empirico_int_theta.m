3da0fa4f3b3a7370ccf57f9f9976f5a9df4b682a
Pablo Saavedra <pablo.saavedra@ug.uchile.cl>
2015-10-22 05:57:33 -0200

    Script para generar la simulacion del control - observador Empirico Kalman

--- exp2/sim_ctrl_obs_empirico_int_theta.m
+++ exp2/sim_ctrl_obs_empirico_int_theta.m
@@ -1,39 +1,59 @@
 % prepare workspace
 clc;
 close all;
-clear;
+clear all;
 format shortG
 format compact
 addpath('lib', 'mat','sim');
 
 load('mat/cubo.mat');
-load('mat/system_int_theta.mat');
 load('mat/linear_sys_int_theta.mat');
-load('mat/discrete_sys_int_theta.mat');
+load('mat/discrete_sys_int_theta.mat'); Cd = [0 1 0 0 0;0 0 1 0 0;0 0 0 1 0];
 
-
-% initial conditions
-x_0 = zeros(x_size, 1);
-x_0(idx_alpha) = pi/90;
-x_0(idx_theta) = pi/90;
+%% Condiciones iniciales
+x_0 = [0, 0, 0, 0, 0];
 
 %% Controladores
-% Calculo ganancia con posicionamiento de polos
-% poles_c = eig(A) - [0 0 15 0 0]';
-% K = place(A, B, poles_c); % posicionamiento de polos
-% Calculo ganancia con LQR 
-Q = diag([1 1 1 1 1]); R = 0.5*diag(1);
-K = lqrd(A, B, Q, R, Ts*0.1);
-Knoise = [0.0001 0.005]; % noise theta and d_alpha
+r = 0;
+load('test_lqr_2_empirico');
+K = lqrd(A, B, Qop, Rop, Nop, Ts);
+
+Knoise = [0.000001 0.005]; % noise theta and d_alpha
 
 % Ld = [L(1,:);eye(3);L(2,:)];
 % Acl = [Ad+Bd*K -Bd*K;zeros(5) Ad+Ld*Cd];
 % eig(Acl)
 
 %% Simulacion
-Tt = 5; % time simulation
+Tt = 25 % time simulation
 % sim('sim/sys_ctrl');
-sim('sim/sys_ctrl_obs_empirico_int_theta');
+% e_obs_alpha_min = 1E10;
+% for Add = 0.1:0.1:1
+%     for Bdd = 10*(0.5 - rand(4,60));
+%         Bdd = Bdd';
+%         sim('sim/sys_ctrl_obs_empirico_2_int_theta');
+%         e_obs_alpha = mean(e_obs(:,2).^2);
+%         if(e_obs_alpha < e_obs_alpha_min)
+%             e_obs_alpha_min = e_obs_alpha;
+%             Add_op = Add;
+%             Bdd_op = Bdd;
+%         end
+%     end
+% end
+
+A22 = Ad(1,1);
+A21 = Ad(1,2:end);
+A11 = Ad(2:end,2:end);
+A12 = Ad(2:end,1);
+B1 = Bd(2:end);
+B2 = Bd(1);
+
+% Kalman
+P0 = pi/100;
+Qd = 0.5;
+Rd = diag([0.001 0.01 1 1]);
+
+sim('sim/sys_ctrl_obs_empirico_2_int_theta');
 EF   = states_feno;
 EF2  = [states_feno(:,1) interp1(states_feno_obs(:,1),...
     states_feno_obs(:,2:end),states_feno(:,1))];
@@ -65,6 +85,6 @@ xlabel('Tiempo [s]'); ylabel('Angulo [rad]');
 title({'Simulacion Modelo Fenomenologico',sprintf('$$ X_0 = [\\alpha = %0.4f, \\theta = %0.4f, \\dot{\\alpha} = %0.4f, \\dot{\\theta} = %0.4f]$$', x0)},'interpreter','latex');
 legend(gca, 'show', 'location', 'best');
 
-plot_error(EF, EF2, 'Error entre Observaciï¿½n y Simulaciï¿½n');
+plot_error(EF, EF2, 'Error entre Observación y Simulación');
 
 
