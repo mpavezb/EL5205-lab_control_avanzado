482bd78c12c47f8fed6e079ba94051ce7227424a
matiasp <matias.pavez.b@gmail.com>
2015-10-30 03:58:29 -0300

    se actualizan controladores

--- exp3/sample.m
+++ exp3/sample.m
@@ -1,13 +1,17 @@
 
 close all;
 clc;
+format shortG
+format compact
 
 addpath('lib');
 
 %% observability/controllability
 clear;
 load('mat/defs.mat');
+
 analyze_sys(tower);
+analyze_sys(jib);
 
 
 %% perfil de perturbacion
@@ -28,6 +32,11 @@ grid on;
 clear;
 load('mat/defs.mat');
 
+t = 0:Ts:2;
+u = zeros(1,length(t));
+u(1:floor(0.5/Ts)) = 0.5;
+u(floor(0.5/Ts)+1:floor(1/Ts)) = -0.5;
+
 Nu = 5;
 xt    = [0; 0; 0; 0];
 y_est = predict_y(tower, xt, u(1:Nu));
@@ -37,9 +46,27 @@ hold on;
 plot(t(1:Nu),y_est,'r');
 grid on;
 
-clear Nu xt y_est
-
 %% control
+%
+% IMPORTANTE: para disenhar los MPCs se recomienda seguir los siguientes 
+% pasos:
+%
+% - >> mpctool
+% - File -> Load -> mat/Tower_MPC_Design_Task.mat
+% - .... trabajar ....
+%
+% # para guardar el Task:
+% - File -> Save -> Elegir proyecto respectivo: "Tower_MPC_Design_Task"
+%     - Almacenar en archivo de mismo nombre en mat.
+%
+% # para actualizar controlador:
+% # desde mpctool elegir
+% - "Controllers" -> Export -> To mat file -> almacenar en archivo:
+%  mat/tower_MPC.mat
+%
+% obs: tambien se puede actualizar el controlador desde el mismo bloque de
+% simulink.
+
 clear;
 load('mat/defs.mat');
 load('mat/tower_MPC.mat');
