a2d695676b30da7c55a0d70305e22c68c091f1d4
matias pavez <matias.pavez.b@gmail.com>
2015-11-18 23:18:50 -0300

    archivos exp3

--- exp3/plot_act1.m
+++ exp3/plot_act1.m
@@ -2,7 +2,7 @@ clc;
 clear;
 close all;
 
-dataLab = load('mat/matlab_test19.mat');
+dataLab = load('mat/matlab_test37.mat');
 
 t         = dataLab.alpha(:,1);
 alpha_ref = dataLab.alpha(:,2);
@@ -13,9 +13,9 @@ theta_ref = dataLab.theta(:,2);
 theta     = dataLab.theta(:,3);
 S_theta   = stepinfo(theta, t);
 
-x_ref     = dataLab.x(:,2);
-x         = dataLab.x(:,3);
-S_x       = stepinfo(x, t);
+%x_ref     = dataLab.x(:,2);
+%x         = dataLab.x(:,3);
+%S_x       = stepinfo(x, t);
 
 u         = dataLab.Im(:,2);
 
@@ -23,42 +23,29 @@ u         = dataLab.Im(:,2);
 %% plot 1
 figure;
 
-h(1) = subplot(311); hold on;
-plot(t, u, '-b', 'displayname', 'u');
-legend(gca, 'show');
-ylabel('Corriente [A]');
-xlim([min(t) max(t)]);
-grid;
+%h(1) = subplot(311); hold on;
+%plot(t, u, '-b', 'displayname', 'u');
+%legend(gca, 'show');
+%ylabel('Corriente [A]');
+%xlim([min(t) max(t)]);
+%grid;
 
-h(2) = subplot(312); hold on;
+subplot(211); hold on;
 plot(t, theta, '-b', 'displayname', '\theta');
 plot(t, theta_ref, '--r', 'displayname', '\theta_{ref}');
 legend(gca, 'show');
-ylabel('Ángulo [deg]');
+ylabel('angulo [deg]');
 xlim([min(t) max(t)]);
 grid;
 
-h(3) = subplot(313); hold on;
+subplot(212); hold on;
 plot(t, alpha, '-b', 'displayname', '\alpha');
 plot(t, alpha_ref, '--r', 'displayname', '\alpha_{ref}');
 legend(gca, 'show');
-xlabel('Tiempo [s]'); ylabel('Ángulo [deg]');
+xlabel('Tiempo [s]'); ylabel('angulo [deg]');
 xlim([min(t) max(t)]);
+ylim([-4 4]);
 grid;
 
-set(h(1),'xticklabel',[]);
-set(h(2),'xticklabel',[]);
-pos=get(h,'position');
-bottom=pos{2}(2);
-top=pos{1}(2)+pos{1}(4);
-plotspace=top-bottom;
-pos{3}(4)=plotspace/2;
-pos{2}(4)=plotspace/2;
-pos{1}(4)=plotspace/2;
-pos{1}(2)=bottom+plotspace/2+0.05;
-pos{2}(2)=pos{3}(2)+plotspace/2+0.05;
-set(h(1),'position',pos{1});
-set(h(2),'position',pos{2});
-set(h(3),'position',pos{3});
-linkaxes(h,'x');
-
+plot([min(t) max(t)],[3 3], '-.k')
+plot([min(t) max(t)],[-3 -3], '-.k')
